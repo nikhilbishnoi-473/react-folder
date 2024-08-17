70e7e7b162e7938d96229e5451b59f1df8727656
nikhilbishnoi <nikhilbishnoibhadugmail.com@nikhilbishnois-MacBook-Air.local>
2024-08-17 12:49:25 +0530

    first commit

---
+++ src/components/views/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import About from '../home/About'
+import Hero from '../home/Hero'
+
+const Home = () => {
+  return (
+   <>
+   <About/>
+   <Hero/>
+   </>
+  )
+}
+
+export default Home
---
+++ src/components/views/Work.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from '../work/Footer'
+import Header from '../work/Header'
+
+const Work = () => {
+    return (
+        <>
+            <Footer />
+            <Header />
+        </>
+    )
+}
+
+export default Work
